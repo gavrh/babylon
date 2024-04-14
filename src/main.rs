dbf818178e36c40a0a812a62a1b46b22b47a106a
gavin <96849112+g-sd@users.noreply.github.com>
2024-04-13 22:43:11 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,12 @@ fn main() {
     
     let mut args: Vec<String> = env::args().collect();
     args.remove(0);
-    let _args = args;
+    let args = args;
     
+    for arg in args {
+        println!("{}", arg);
+    }
+
     // tbc
 
 }
