2a0a644de930747bba1bbe3ceff4b71184c27b5d
gavin <96849112+g-sd@users.noreply.github.com>
2024-04-14 13:41:21 -0700

    remove printing args

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,8 @@ fn main() {
     
     let mut args: Vec<String> = env::args().collect();
     args.remove(0);
-    let args = args;
+    let _args = args;
     
-    for arg in args {
-        println!("{}", arg);
-    }
-
     // tbc
 
 }
