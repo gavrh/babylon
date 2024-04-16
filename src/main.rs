a3a42c2e734c4ac77452b93aed7032a182e5e674
gavin <96849112+g-sd@users.noreply.github.com>
2024-04-16 16:55:36 -0700

    server setup

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,48 @@
-use std::env;
+use std::net::TcpListener;
+use std::net::TcpStream;
+use std::io::prelude::Read;
+use std::io::prelude::Write;
+use std::fs;
+use std::thread;
+
 
 fn main() {
-    
-    let mut args: Vec<String> = env::args().collect();
-    args.remove(0);
-    let args = args;
-    
-    // view args (temp)
-    for arg in args {
-        println!("{}", arg);
+
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        thread::spawn(|| {
+            handle_connection(stream);
+        });
     }
 
-    // tbc
+}
 
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+    println!("Req: {}", String::from_utf8_lossy(&buffer[..]));
+
+    // HTTP-Version Status-Code Reason-Phrase CRLF
+    // headers CRLF
+    // message-body
+    //
+    // ex: HTTP/1.1 200 OK\r\n\r\n
+
+    let get = b"GET / HTTP/1.1\r\n";
+    if buffer.starts_with(get) {
+        let content = fs::read_to_string("index.html").unwrap();
+
+        let response = format!(
+            "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+            content.len(),
+            content
+        );
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+
+    }
 }
